4ddc06cb2dd9e7290484b77faff0310255d0d125
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-07-22 15:06:44 +0300

    all issues have been corrected

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 class Car:
-    def __init__(self, comfort_class: int,
+    def __init__(self,
+                 comfort_class: int,
                  clean_mark: int,
                  brand: str) -> None:
-        if 1 <= comfort_class <= 7:
-            self.comfort_class = comfort_class
-
-        if 1 <= clean_mark <= 10:
-            self.clean_mark = clean_mark
-
+        self.comfort_class = comfort_class if 1 <= comfort_class <= 7 else 1
+        self.clean_mark = clean_mark if 1 <= clean_mark <= 10 else 1
         self.brand = brand
 
 
@@ -17,32 +14,36 @@ class CarWashStation:
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
-        if 1.0 <= distance_from_city_center <= 10.0:
-            self.distance_from_city_center = distance_from_city_center
-
+        self.distance_from_city_center = (
+            distance_from_city_center
+            if 1.0 <= distance_from_city_center <= 10.0
+            else 1.0
+        )
         self.clean_power = clean_power
-
-        if 1.0 <= average_rating <= 5.0:
-            self.average_rating = average_rating
-
+        self.average_rating = (
+            average_rating
+            if 1.0 <= average_rating <= 5.0
+            else 1.0
+        )
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating
-                     / self.distance_from_city_center, 1)
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center, 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
-        if self.count_of_ratings == 0:
-            self.count_of_ratings = 1
-            self.average_rating = rate
-        self.average_rating = round((self.average_rating
-                                     * self.count_of_ratings + rate)
-                                    / (self.count_of_ratings + 1), 1)
+        self.average_rating = round(
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1),
+            1
+        )
         self.count_of_ratings += 1
 
     def serve_cars(self, cars: list[Car]) -> float:
